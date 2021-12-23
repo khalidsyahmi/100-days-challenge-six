5c0fd0d0f8997aa6dc81ad4f2fcad4e242521794
Khalid_Syahmi <mksyahmiof@gmail.com>
2021-12-23 12:01:41 +0800

    JS-2

--- functions-and-parameters.js
+++ functions-and-parameters.js
@@ -55,7 +55,7 @@ alert(bootCamp.campGoals[2]);
 function getCampList(justArray, justIndex) {
   /* parameter (unique) */
 
-  /* the logic behind this is forming function of retrieving a variable in an existing array*/
+  /* the logic behind this is forming function of retrieving a property in an existing array*/
   /* which is already grouped in an obj */
   let justArrayVariable = justArray[justIndex];
   return justArrayVariable;
@@ -74,18 +74,6 @@ alert(firstGoal);
 
 // Exercise Time!
 
-// 1) Create three new variables:
-//    - A variable that stores the name of an online course of your choice
-//    - A variable that stores the price of that course
-//    - A variable that stores the three main goals that you have, when taking this course
-// 2) Output ("alert") the three variable values
-// 3) Try "grouping" the three variables together and still output their values thereafter
-// 4) Also output the second element in your "main goals" variable
-// 5) Add a custom command that does the following:
-//    - Use your "main goals" variable and access an element by its identifier
-//    - The concrete identifier value should be dynamic / flexible
-//      (i.e. the command can be executed for different identifier)
-//    - The "main goals" variable should also be dynamic: The command should work
-//      with ANY list of values
-//    - The custom command should provide the accessed value (i.e. the list element)
-// 6) Execute your custom command from (5) and output ("alert") the result
+/* variable in obj = property
+  function in obj = method
+*/
